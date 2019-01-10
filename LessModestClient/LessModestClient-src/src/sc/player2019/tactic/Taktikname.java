6a23839ac736502ec3489e32699586a735fe35b5
Kayost Layton <31962076+Kayost-Layton@users.noreply.github.com>
2019-01-10 14:39:07 +0100

    Update Taktikname.java

--- LessModestClient/LessModestClient-src/src/sc/player2019/tactic/Taktikname.java
+++ LessModestClient/LessModestClient-src/src/sc/player2019/tactic/Taktikname.java
@@ -1,7 +1,15 @@
 package sc.player2019.tactic;
 
-public enum Taktikname {
+public enum Taktiken {
 	
-	DEFAULT, RANDOM, MITTE, MINSPREAD
+	/*DEFAULT, RANDOM,*/ MITTE(new TacticMitte());//, MINSPREAD;
+	
+	Tactic tactic;
+	Taktiken(Tactic tactic){
+		this.tactic = tactic;
+	}
+	public Tactic getTactic(){
+		return tactic;
+	}
 	
 }
